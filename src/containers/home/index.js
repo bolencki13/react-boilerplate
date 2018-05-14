32bf80ec45b57ccd73687adb4cf444f3d500665c
Brian Olencki <bolencki@its.jnj.com>
2018-05-14 10:18:12 -0400

    basic home page container

---
+++ src/containers/home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import App from 'components/app'
+
+class HomePage extends Component {
+  render () {
+    return (
+      <App/>
+    )
+  }
+}
+
+export default HomePage
