f7ef2f79fddeaad891746a7df332d171f95af699
Brian Olencki <bolencki@its.jnj.com>
2018-05-14 11:59:12 -0400

    file loading

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import HomePage from 'containers/home'
+import registerServiceWorker from './registerServiceWorker'
 
 import 'stylesheets/index.scss'
+
+registerServiceWorker()
 
 ReactDOM.render(
   <HomePage/>,
