81c45d1dd1e1a6b2bd8ecb6c8d5efe9c28ccceec
Brian Olencki <bolencki@its.jnj.com>
2018-05-14 10:59:16 -0400

    basic scss|sass support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import HomePage from 'containers/home'
 
+import 'stylesheets/index.scss'
+
 ReactDOM.render(
   <HomePage/>,
   document.getElementById('app')
